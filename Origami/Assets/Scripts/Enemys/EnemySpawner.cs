1bea97cdad58a618bd306ed16f8dddf077cd23fa
JordanWhyte1 <48695976+JordanWhyte1@users.noreply.github.com>
2019-05-28 14:52:39 -0700

    Preliminary creation of variable amounts and types of enemies on each wave

--- Origami/Assets/Scripts/Enemys/EnemySpawner.cs
+++ Origami/Assets/Scripts/Enemys/EnemySpawner.cs
@@ -28,13 +28,25 @@ public class EnemySpawner : MonoBehaviour
     public int EnemysPerWave = 10;
     private int numActiveEnemy = 0;
 
+    public int waveNumber = 1;
+
     public float timeBetweenSpawns = 0.2f;
 
     private float timer = 0.0f;
     private bool waveSpawn = true;
 
+    public bool waveStart = false;
+
+    private bool wave5 = false;
+    private bool wave10 = false;
+    private bool wave15 = false;
+    private bool wave20 = false;
+    private bool wave25 = false;
+
+
     void Start()
     {
+
     }
 
     void Update()
@@ -72,6 +84,98 @@ public class EnemySpawner : MonoBehaviour
         {
             // disables the wave spawner
             waveSpawn = false;
+            waveNumber += 1;
+            if (waveNumber <= 5)
+            {
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 100;
+                Enemys[1].SpawnIDMin = 0;
+                Enemys[1].SpawnIDMax = 0;
+                Enemys[2].SpawnIDMin = 0;
+                Enemys[2].SpawnIDMax = 0;
+                EnemysPerWave += 2;
+            }
+            else if (waveNumber > 5 && waveNumber <= 10)
+            {
+                if (wave5 == false)
+                {
+                    EnemysPerWave = 10;
+                    wave5 = true;
+                }
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 66;
+                Enemys[1].SpawnIDMin = 66;
+                Enemys[1].SpawnIDMax = 100;
+                Enemys[2].SpawnIDMin = 0;
+                Enemys[2].SpawnIDMax = 0;
+
+                EnemysPerWave += 3;
+            }
+            else if (waveNumber > 10 && waveNumber <= 15)
+            {
+                if (wave10 == false)
+                {
+                    EnemysPerWave = 10;
+                    wave10 = true;
+                }
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 33;
+                Enemys[1].SpawnIDMin = 33;
+                Enemys[1].SpawnIDMax = 66;
+                Enemys[2].SpawnIDMin = 66;
+                Enemys[2].SpawnIDMax = 100;
+                EnemysPerWave += 3;
+
+            }
+            else if (waveNumber > 15 && waveNumber <= 20)
+            {
+                if (wave15 == false)
+                {
+                    EnemysPerWave = 5;
+                    wave15 = true;
+                }
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 0;
+                Enemys[1].SpawnIDMin = 0;
+                Enemys[1].SpawnIDMax = 50;
+                Enemys[2].SpawnIDMin = 50;
+                Enemys[2].SpawnIDMax = 100;
+                EnemysPerWave += 2;
+            }
+            else if (waveNumber > 20 && waveNumber <= 25)
+            {
+                if (wave20 == false)
+                {
+                    EnemysPerWave = 10;
+                    wave20 = true;
+                }
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 50;
+                Enemys[1].SpawnIDMin = 0;
+                Enemys[1].SpawnIDMax = 0;
+                Enemys[2].SpawnIDMin = 50;
+                Enemys[2].SpawnIDMax = 100;
+                EnemysPerWave += 2;
+            }
+            else if (waveNumber > 25 && waveNumber <= 30)
+            {
+                if (wave25 == false)
+                {
+                    EnemysPerWave = 15;
+                    wave25 = true;
+                }
+                Enemys[0].SpawnIDMin = 0;
+                Enemys[0].SpawnIDMax = 33;
+                Enemys[1].SpawnIDMin = 33;
+                Enemys[1].SpawnIDMax = 66;
+                Enemys[2].SpawnIDMin = 66;
+                Enemys[2].SpawnIDMax = 100;
+                EnemysPerWave += 2;
+            }
+            else if (waveNumber > 30)
+            {
+                EnemysPerWave += (waveNumber / 8);
+            }
         }
     }
 
